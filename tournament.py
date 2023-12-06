ede00b3b661ed410aee7538ac6d417b3c83170d5
Travis Clark <travis.scott.clark@gmail.com>
2023-12-05 20:27:31 -0500

    Non-mirrored win rate

--- tournament.py
+++ tournament.py
@@ -16,6 +16,53 @@ def get_elo_of_tournament_players(name):
         total_player_list.extend(player_list)
     return total_player_list
 
+def calculate_not_match_faction_win_rate(name):
+    empire_wins = 0
+    empire_loses = 0
+    rebels_wins = 0
+    rebels_loses = 0
+    republic_wins = 0
+    republic_loses = 0
+    separatists_wins = 0
+    separatists_loses = 0
+    mercenary_wins = 0
+    mercenary_loses = 0
+    player_id_faction_dict = {}
+    
+    groups = service.get_tournament_groups(name)
+    if groups:
+        for group in groups:
+            players = group["players"]
+            for player in players:
+                if player["legionList"]:
+                    player_id_faction_dict[int(player["id"])] = player["legionList"]["faction"]
+            for round in group["rounds"]:
+                for match in round["matches"]:
+                    if not match["isBye"] and match["winner"]:
+                        winner_id = int(match["winner"]["id"])
+                        loser_id = int(match["loser"]["id"])
+                        if (player_id_faction_dict[winner_id] != player_id_faction_dict[loser_id]):
+                            match player_id_faction_dict[winner_id]:
+                                case "EMPIRE": empire_wins += 1
+                                case "REBELS": rebels_wins += 1
+                                case "REPUBLIC": republic_wins += 1
+                                case "SEPARATISTS": separatists_wins += 1
+                                case "MERCENARY": mercenary_wins += 1
+                            match player_id_faction_dict[loser_id]:
+                                case "EMPIRE": empire_loses += 1
+                                case "REBELS": rebels_loses += 1
+                                case "REPUBLIC": republic_loses += 1
+                                case "SEPARATISTS": separatists_loses += 1
+                                case "MERCENARY": mercenary_loses += 1
+                            
+    empire_win_rate = 100 * empire_wins / (empire_wins + empire_loses)
+    rebels_win_rate = 100 * rebels_wins / (rebels_wins + rebels_loses)
+    republic_win_rate = 100 * republic_wins / (republic_wins + republic_loses)
+    separatists_win_rate = 100 * separatists_wins / (separatists_wins + separatists_loses)
+    mercenary_win_rate = 100 * mercenary_wins / (mercenary_wins + mercenary_loses)
+    print(f"Empire: {empire_wins}-{empire_loses}, Rebels: {rebels_wins}-{rebels_loses}, Republic: {republic_wins}-{republic_loses}, Separatists: {separatists_wins}-{separatists_loses}, Mercenary: {mercenary_wins}-{mercenary_loses}")
+    print(f"Empire: {empire_win_rate}, Rebels: {rebels_win_rate}, Republic: {republic_win_rate}, Separatists: {separatists_win_rate}, Mercenary: {mercenary_win_rate}")
+
 def update_tournament_data(name):
     groups = service.get_tournament_groups(name)
     if groups:
@@ -107,11 +154,12 @@ def get_game_results(db_player, player):
     return db_player
 
 def main():
-    #  update_tournament_data("las-vegas-open-grand-championship")
+    # update_tournament_data("star-wars-legion-wq-at-pax-unplugged-2023")
     # update_tournament_data("atomic-empire-star-wars-legion-tournament-darkness-descends")
-    player_list = get_elo_of_tournament_players("las-vegas-open-grand-championship")
-    sorted_player_list = sorted(player_list, key=lambda i: i["weighted_elo"], reverse=True)
-    print(sorted_player_list)
+    # player_list = get_elo_of_tournament_players("star-wars-legion-wq-at-pax-unplugged-2023")
+    # sorted_player_list = sorted(player_list, key=lambda i: i["weighted_elo"], reverse=True)
+    # print(sorted_player_list)
+    calculate_not_match_faction_win_rate("star-wars-legion-wq-at-pax-unplugged-2023")
     
 if __name__ == '__main__':
     # cProfile.run('main()')
