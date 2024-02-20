aecb019ba039096353d9acaddfa5c3e561bbaf70
Travis Clark <travis.scott.clark@gmail.com>
2024-02-20 10:42:41 -0500

    Newest updates to json

--- tournaments.py
+++ tournaments.py
@@ -12,8 +12,8 @@ def update_recent_elos(start_date, end_date):
         tournament.update_tournament_data(tournament_name)
         
 def main():
-    update_recent_elos("2022-11-01 12:00:00", "2023-12-04 01:00:00")
-    # update_recent_elos("2023-11-20 01:00:00", "2023-12-04 01:00:00")
+    # update_recent_elos("2022-11-01 12:00:00", "2023-12-04 01:00:00")
+    update_recent_elos("2023-12-04 01:00:00", "2024-02-20 01:00:00")
     players = db.get_all()
     weightedPlayers = db.get_all_sorted("weighted_elo")
     with open("getAll.json", "w", encoding='utf8') as file:
